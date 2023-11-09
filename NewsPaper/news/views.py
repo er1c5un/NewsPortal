51c1e0a3b07bc60dde0628a53878114484bf9cf0
eric <ericrad@bk.ru>
2023-11-09 23:41:24 +0300

    Итоговое задание 9.5.4 (HW-03): Добавил шаблон для рассылки при добавлении нового поста

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.contrib.auth.models import Group
 from django.core.mail import send_mail
 from django.shortcuts import redirect
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 from django.views.generic import ListView, UpdateView, CreateView, DetailView, \
     DeleteView
 
@@ -65,12 +67,18 @@ class PostCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
         email_list = []
         for sub in subscribers:
             email_list.append(sub.user.email)
+        context = {
+            'post': new_post,
+        }
+        html_message = render_to_string('account/email/email_new_post.html', context)
+        plain_message = strip_tags(html_message)
         # отправляем письмо всем админам по аналогии с send_mail, только здесь получателя указывать не надо
         send_mail(
             subject=f'{new_post.title}',  # имя клиента и дата записи будут в теме для удобства
-            message=f"{new_post.text}",  # сообщение с кратким описанием проблемы
+            message=plain_message,  # сообщение с кратким описанием проблемы
             from_email='er1c5un@yandex.ru',  # здесь указываете почту, с которой будете отправлять (об этом попозже)
-            recipient_list=email_list  # здесь список получателей. Например, секретарь, сам врач и т. д.
+            recipient_list=email_list,  # здесь список получателей. Например, секретарь, сам врач и т. д.
+            html_message=html_message,
         )
 
         return redirect('/')
